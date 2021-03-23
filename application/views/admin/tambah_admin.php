532b688a874c2d8220475588a6584fbc6902881f
Bahrum <81005654+bahrum12@users.noreply.github.com>
2021-03-23 16:13:56 +0700

    Update tambah_admin.php

--- application/views/admin/tambah_admin.php
+++ application/views/admin/tambah_admin.php
@@ -1,17 +1,53 @@
 <html>
 <head>
+<style>
+
+</style>
+
 <title></title>
+<link rel="stylesheet" href="<?=base_url();?>assets/bootstrap/css/bootstrap.min.css">
+
 </head>
-<body>
-    <h1>Tambah Admin</h1>
+    <body class="d-flex flex-column h-100">
+    <!-- Page Content -->
+<main role="main" class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5">Tambah Admin</h1>
+    <p class="lead">Silahkan Daftarkan Identitas Anda</p>
+    <hr/>
     <form action="<?=base_url()?>pemilwa/addadmin" method="post">
-    <label for="nama_admin">Nama Admin</label>
-    <input type="text" name="nama_admin" id="nama_admin">
-    <label for="username">Username</label>
-    <input type="text" name="username" id="username">
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password">
-    <input type="submit" value="Simpan">
-    </form>
+    <div class="form-group row">
+      <label for="username" class="col-sm-2 col-form-label">Username</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+      </div>
+    </div>
+     <div class="form-group row">
+      <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama">
+      </div>
+    </div>
+  <div class="form-group row">
+    <label for="password" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <a href="login.php" class="btn btn-success">Login</a>
+      <button type="submit" class="btn btn-primary" name="register">Register</button>
+    </div>
+  </div>
+</form>
+  </div>
+</main>
+<footer class="footer mt-auto py-3">
+  <div class="container">
+
+  </div>
+</footer>
 </body>
+
 </html>
