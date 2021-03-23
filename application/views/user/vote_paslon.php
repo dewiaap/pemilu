b7422ec34b6cc143e858539ad708afe5b48c48df
Thoriq210800 <44075782+Thoriq210800@users.noreply.github.com>
2021-03-23 07:42:09 +0700

    pembenahan form

--- application/views/user/vote_paslon.php
+++ application/views/user/vote_paslon.php
@@ -8,7 +8,8 @@
     <h2>Paslon BEM</h2>
     <?php
     echo '<ul>';
-    foreach ($bem as $b) { ?>
+	<form method="post" action="<?=base_url()?>pemilwa/vote_paslon">
+    	foreach ($bem as $b) { ?>
             <li>
                 <img src="<?= base_url() ?>/assets/image/paslon/<?= $b->gambar ?>">
                 <p>nama pasangan : <?= $b->nama_pasangan ?></p>
@@ -17,13 +18,11 @@
                 <!-- kalo bisa dibuat drop down untuk visi misi, kalo nggak ya dihapus gakpapa atau gimana -->
                 <p>visi : <?= $b->visi ?></p>
                 <p>misi : <?= $b->misi ?>
-                <form method="post" action="<?=base_url()?>pemilwa/vote_paslon">
-                    <input type="radio" name="dipilih" value="<?= $b->no_urut ?>">
-                    <input type="submit" name="dipilih">Pilih
-                </form>
-                <p>
+                <input type="radio" name="dipilih" value="<?= $b->no_urut ?>">
             </li>
-    <?php } ?>
+    	<?php } ?>
+		<input type="submit" name="dipilih">Pilih
+	</form>
     </ul>
 
 
