ec72a8062ff1389a2e13e3630c6c1d7f07e152a2
Thoriq210800 <44075782+Thoriq210800@users.noreply.github.com>
2021-03-23 13:02:53 +0700

    nambah view terimakasih setelah memilih

---
+++ application/views/user/terimakasih.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+    <h1>Terimakasih telah berpartisipasi dalam PEMILWA tahun 2021</h1>
+    <label><a href="<?= base_url() ?>pemilwa/logout">Logout</a></label>
+</body>
+</html>
