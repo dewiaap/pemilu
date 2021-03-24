43e2a2f2f055e960b82cd3238e9758a085038674
Bahrum <81005654+bahrum12@users.noreply.github.com>
2021-03-24 16:07:06 +0700

    Update ubah_calonbpm.php

--- application/views/admin/ubah_calonbpm.php
+++ application/views/admin/ubah_calonbpm.php
@@ -1,28 +1,71 @@
 <html>
 <head>
 <title></title>
+<link rel="stylesheet" href="<?=base_url();?>assets/bootstrap/css/bootstrap.min.css">
+
 </head>
-<body>
-    <h1>Update Calon BPM</h1>
+    <body class="d-flex flex-column h-100">
+    <!-- Page Content -->
+<main role="main" class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5">Update Calon BPM</h1>
+    <p class="lead">Silahkan Daftarkan Identitas Anda</p>
+    <hr/>
     <form action="<?=base_url()?>pemilwa/updatebpm" method="post" enctype="multipart/form-data">
+    <div class="form-group row">
     <input type="hidden" name="no_urut" value=<?=$calon->no_urut?>>
-    <label for="nama_lengkap">Nama Lengkap</label>
-    <input type="text" name="nama_lengkap" id="nama_lengkap" value=<?=$calon->nama_lengkap?>>
-    <br><label for="gambar">Gambar</label>
-    <input type="file" name="gambar" id="gambar">
-    <br><label for="nim">Nim</label>
-    <input type="text" name="nim" id="nim" value=<?=$calon->nim?>>
-    <br><label for="id_prodi">Prodi</label>
-    <select name="id_prodi" id="id_prodi">
-    <?php foreach($prodi as $p){?>
-    <option value="<?=$p->id_prodi?>"<?php if($p->id_prodi == $calon->id_prodi){echo "selected";}?>><?=$p->prodi?></option>
-    <?php } ?>
-    </select>
-    <br><label for="visi">Visi</label>
-    <textarea name="visi" id="visi" cols="30" rows="10"><?=$calon->visi?></textarea>
-    <br><label for="misi">Misi</label>
-    <textarea name="misi" id="misi" cols="30" rows="10"><?=$calon->misi?></textarea>
-    <br><input type="submit" value="Simpan">
-    </form>
+    </div>
+    <div class="form-group row">
+      <label for="nama_lengkap" class="col-sm-2 col-form-label">Nama Lengkap</label>
+      <div class="col-sm-10">
+        <input type="text" name="nama_lengkap" id="nama_lengkap" class="form-control" placeholder="Masukkan Nama Lengkap" value=<?=$calon->nama_lengkap?> required>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="gambar" class="col-sm-2 col-form-label">Gambar</label>
+      <div class="col-sm-10">
+        <input type="file" name="gambar" id="gambar">
+      </div>
+    </div>
+    <hr/>
+    <div class="form-group row">
+      <label for="nim" class="col-sm-2 col-form-label">NIM</label>
+      <div class="col-sm-10">
+        <input type="text" name="nim" id="nim" class="form-control" placeholder="NIM" value=<?=$calon->nim?> required> 
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="id_prodi" class="col-sm-2 col-form-label">Prodi</label>
+      <div class="col-sm-10">
+      <select name="id_prodi" id="id_prodi">
+        <?php foreach($prodi as $p){?>
+        <option value="<?=$p->id_prodi?>"<?php if($p->id_prodi == $calon->id_prodi){echo "selected";}?>><?=$p->prodi?></option>
+        <?php } ?>
+      </select>
+      </div>
+    </div>
+    <hr/>
+    <div class="form-group row">
+    <label for="visi" class="col-sm-2 col-form-label">Visi</label>
+    <div class="col-sm-10">
+    <textarea name="visi" id="visi" cols="130" rows="10" required><?=$calon->visi?></textarea>
+    </div>
+  </div>
+  <hr/>
+  <div class="form-group row">
+    <label for="misi" class="col-sm-2 col-form-label">Misi</label>
+    <div class="col-sm-10">
+    <textarea name="misi" id="misi" cols="130" rows="10" required><?=$calon->misi?></textarea>
+    </div>
+  </div>
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <button type="submit" class="btn btn-primary" name="register">Simpan</button>
+    </div>
+  </div>
+</form>
+  </div>
+</main>
 </body>
+
 </html>
