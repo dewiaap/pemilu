7ade0ea5d4c06c502d54af98ba416542b78d8eac
Bahrum <81005654+bahrum12@users.noreply.github.com>
2021-03-24 07:08:20 +0700

    Update tambah_paslon.php

--- application/views/admin/tambah_paslon.php
+++ application/views/admin/tambah_paslon.php
@@ -1,35 +1,89 @@
 <html>
 <head>
+<style>
+
+</style>
+
 <title></title>
+<link rel="stylesheet" href="<?=base_url();?>assets/bootstrap/css/bootstrap.min.css">
+
 </head>
-<body>
-    <h1>Tambah Paslon</h1>
+    <body class="d-flex flex-column h-100">
+    <!-- Page Content -->
+<main role="main" class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5">Tambah Paslon</h1>
+    <p class="lead">Silahkan Daftarkan Identitas Anda</p>
+    <hr/>
     <form action="<?=base_url()?>pemilwa/addpaslon" method="post" enctype="multipart/form-data">
-    <label for="nama_pasangan">Nama Pasangan</label>
-    <input type="text" name="nama_pasangan" id="nama_pasangan">
-    <br><label for="gambar">Gambar</label>
-    <input type="file" name="gambar" id="gambar">
-    <br><label for="nim_ketua">Nim Ketua</label>
-    <input type="text" name="nim_ketua" id="nim_ketua" required>
-    <br><label for="id_prodi_ketua">Prodi Ketua</label>
-    <select name="id_prodi_ketua" id="id_prodi_ketua" required>
-    <?php foreach($prodi as $p){?>
-    <option value="<?=$p->id_prodi?>"><?=$p->prodi?></option>
-    <?php } ?>
-    </select>
-    <br><label for="nim_wakil">Nim Wakil</label>
-    <input type="text" name="nim_wakil" id="nim_wakil" required>
-    <br><label for="id_prodi_wakil">Prodi Wakil</label>
-    <select name="id_prodi_wakil" id="id_prodi_wakil" required>
-    <?php foreach($prodi as $p){?>
-    <option value="<?=$p->id_prodi?>"><?=$p->prodi?></option>
-    <?php } ?>
-    </select>
-    <br><label for="visi">Visi</label>
-    <textarea name="visi" id="visi" cols="30" rows="10" required></textarea>
-    <br><label for="misi">Misi</label>
-    <textarea name="misi" id="misi" cols="30" rows="10" required></textarea>
-    <br><input type="submit" value="Simpan">
-    </form>
+    <div class="form-group row">
+      <label for="nama_pasangan" class="col-sm-2 col-form-label">Nama Pasangan</label>
+      <div class="col-sm-10">
+        <input type="text" name="nama_pasangan" id="nama_pasangan" class="form-control" placeholder="Nama">
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="gambar" class="col-sm-2 col-form-label">Gambar</label>
+      <div class="col-sm-10">
+        <input type="file" name="gambar" id="gambar">
+      </div>
+    </div>
+    <hr/>
+    <div class="form-group row">
+      <label for="nim_ketua" class="col-sm-2 col-form-label">NIM Ketua</label>
+      <div class="col-sm-10">
+        <input type="text" name="nim_ketua" id="nim_ketua" class="form-control" placeholder="Nama Ketua" required> 
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="id_prodi_ketua" class="col-sm-2 col-form-label">Prodi Ketua</label>
+      <div class="col-sm-10">
+        <select name="id_prodi_ketua" id="id_prodi_ketua" requireq>
+        <?php foreach($prodi as $p){?>
+        <option value="<?=$p->id_prodi?>"><?=$p->prodi?></option>
+        <?php } ?>
+        </select>
+      </div>
+    </div>
+    <hr/>
+    <div class="form-group row">
+      <label for="nim_wakil" class="col-sm-2 col-form-label">NIM Wakil</label>
+      <div class="col-sm-10">
+        <input type="text" name="nim_wakil" id="nim_wakil" class="form-control" placeholder="Nama Wakil Ketua" required> 
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="id_prodi_wakil" class="col-sm-2 col-form-label">Prodi Wakil</label>
+      <div class="col-sm-10">
+        <select name="id_prodi_wakil" id="id_prodi_wakil" required>
+        <?php foreach($prodi as $p){?>
+        <option value="<?=$p->id_prodi?>"><?=$p->prodi?></option>
+        <?php } ?>
+        </select>
+      </div>
+    </div>
+    <hr/>
+    <div class="form-group row">
+    <label for="visi" class="col-sm-2 col-form-label">Visi</label>
+    <div class="col-sm-10">
+    <textarea name="visi" id="visi" cols="130" rows="10" required></textarea>
+    </div>
+  </div>
+  <hr/>
+  <div class="form-group row">
+    <label for="misi" class="col-sm-2 col-form-label">Misi</label>
+    <div class="col-sm-10">
+    <textarea name="misi" id="misi" cols="130" rows="10" required></textarea>
+    </div>
+  </div>
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <button type="submit" class="btn btn-primary" name="register">Simpan</button>
+    </div>
+  </div>
+</form>
+  </div>
+</main>
 </body>
+
 </html>
