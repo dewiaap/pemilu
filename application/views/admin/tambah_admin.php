f2e9291fcf0c9cda1070610575a2996533df1bd5
Bahrum <81005654+bahrum12@users.noreply.github.com>
2021-03-24 16:24:30 +0700

    Update tambah_admin.php

--- application/views/admin/tambah_admin.php
+++ application/views/admin/tambah_admin.php
@@ -19,19 +19,19 @@
     <div class="form-group row">
       <label for="username" class="col-sm-2 col-form-label">Username</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
+        <input type="text" class="form-control" id="username" name="username" placeholder="Masukkan Username" required>
       </div>
     </div>
      <div class="form-group row">
       <label for="nama" class="col-sm-2 col-form-label">Nama</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" id="nama_admin" name="nama_admin" placeholder="Nama" required>
+        <input type="text" class="form-control" id="nama_admin" name="nama_admin" placeholder="Masukkan Nama" required>
       </div>
     </div>
   <div class="form-group row">
     <label for="password" class="col-sm-2 col-form-label">Password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+      <input type="password" class="form-control" id="password" name="password" placeholder="Masukkan Password" required>
     </div>
   </div>
   <div class="form-group row">
