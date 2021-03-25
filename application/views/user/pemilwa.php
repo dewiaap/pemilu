833c185e226e63cdaa6e095ca31028f3590bd518
Bahrum <81005654+bahrum12@users.noreply.github.com>
2021-03-25 22:23:50 +0700

    Update pemilwa.php

--- application/views/user/pemilwa.php
+++ application/views/user/pemilwa.php
@@ -2,6 +2,13 @@
 <head>
 <title></title>
 <link rel="stylesheet" href="<?=base_url();?>assets/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?=base_url();?>assets/css/style.css">
+
+<style type="text/css">
+      body{
+        background-image:url('<?=base_url();?>assets/image/Astronomy.jpg');
+      }
+    </style>
 </head>
 
 <body>
@@ -17,30 +24,60 @@
     </ul>
   </div>
 </nav>
-    <h1>Selamat Datang di Website Pemilwa 2021</h1>
-    <h2>Yuk, lihat para calon pemimpin filkom!</h2>
-    <h3>Paslon BEM</h3>
-    <?php 
-        foreach($bem as $b){?>
-        <p>nama pasangan : <?= $b->nama_pasangan?></p>
-		<p>nim ketua : <?= $b->nim_ketua?></p>
-		<p>nim wakil : <?= $b->nim_wakil?></p>
-		<p>prodi ketua : <?= $prodi[$b->id_prodi_ketua-1]->prodi?></p>
-		<p>prodi wakil : <?= $prodi[$b->id_prodi_wakil-1]->prodi?></p>
-		<img src="<?=base_url()?>/assets/image/paslon/<?=$b->gambar?>">
-        <p>visi : <?= $b->visi?></p>
-        <p>misi : <?= $b->misi?><p>
+<div id="kontainer">
+        <div class="row">
+            <div class="col-sm-12 text-center" id="TempatTulisan">
+                <h1 id="Selamat"><img class="hidden-xs hidden-sm" src="assets/image/poros1.png">Selamat Datang di Website Pemilwa 2021<img class="hidden-xs hidden-sm" src="assets/image/ub1.png"></h1>
+                <h4 id="Tulisan">"Yuk, lihat para calon pemimpin filkom!"</h4></div>
+        </div>
+<div class="container">
+<h3>Paslon BEM</h3>
+<div class="box" id="databem">
+      
+<div class="row">
+<?php foreach($bem as $b){?>
+              <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+                <img src="<?=base_url()?>/assets/image/paslon/<?=$b->gambar?>">
+              </div>
+              <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 column">
+                <h5 id="pasangan"> <b><?= $b->nama_pasangan?></b></h5> 
+              </div>
+              <div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-7 col-sm-offset-3 column">
+              </div>
+          </div>		
+        <h5 id="pasangan">Visi</h5>
+        <p><?= $b->visi?></p>
+        <h5 id="pasangan">Misi</h5>
+        <p><?= $b->misi?></p>
         <?php }?>
-    
-        <h3>Calon BPM</h3>
-    <?php 
-        foreach($bpm as $b){?>
-        <p>nama lengkap : <?= $b->nama_lengkap?></p>
-		<p>nim : <?= $b->nim?></p>
-		<p>prodi : <?= $prodi[$b->id_prodi-1]->prodi?></p>
-		<img src="<?=base_url()?>/assets/image/bpm/<?=$b->gambar?>">
-        <p>visi : <?= $b->visi?></p>
-        <p>misi : <?= $b->misi?><p>
+</div>
+</div>
+        
+<div class="container">
+<h3>Calon BPM</h3>
+<div class="box" id="databem">
+      
+<div class="row">
+<?php foreach($bpm as $b){?>
+              <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+                <img src="<?=base_url()?>/assets/image/bpm/<?=$b->gambar?>">
+              </div>
+              <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 column">
+                <h5 id="pasangan"> <b><?= $b->nama_lengkap?></b></h5> 
+              </div>
+              <div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-7 col-sm-offset-3 column">
+              </div>
+          </div>		
+        <h5 id="pasangan">Visi</h5>
+        <p><?= $b->visi?></p>
+        <h5 id="pasangan">Misi</h5>
+        <p><?= $b->misi?></p>
         <?php }?>
+</div>
+</div>
+<div class="footer">
+   ©Copyright by Thoriq
+   </div>
+</div>
 </body>
 </html>
