1a72a619a1b140e01fe724eca5265b5ee675788a
Bahrum <81005654+bahrum12@users.noreply.github.com>
2021-03-25 22:17:08 +0700

    Update terimakasih.php

--- application/views/user/terimakasih.php
+++ application/views/user/terimakasih.php
@@ -1,9 +1,19 @@
 <html>
 <head>
-<title></title>
+<link rel="stylesheet" href="<?=base_url();?>assets/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?=base_url();?>assets/css/style.css">
+<style type="text/css">
+      body{
+        background-image:url('<?=base_url();?>assets/image/Astronomy.jpg');
+      }
+    </style>
+<title>Log Out</title>
 </head>
 <body>
+    <div class="col-sm-12 text-center" id="log">
     <h1>Terimakasih telah berpartisipasi dalam PEMILWA tahun 2021</h1>
-    <label><a href="<?= base_url() ?>pemilwa/logout">Logout</a></label>
+    <label><a href="<?= base_url() ?>pemilwa/logout">Logout--></a></label>
+    </div>
+    
 </body>
 </html>
