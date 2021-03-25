58443ede44f84102969ac632572d61a1832b4afd
Bahrum <81005654+bahrum12@users.noreply.github.com>
2021-03-25 22:21:46 +0700

    Update vote_bpm.php

--- application/views/user/vote_bpm.php
+++ application/views/user/vote_bpm.php
@@ -3,27 +3,54 @@
 <head>
     <title></title>
 </head>
-
+<link rel="stylesheet" href="<?=base_url();?>assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/css/style.css">
+    <style type="text/css">
+      body{
+        background-image:url('<?=base_url();?>assets/image/Astronomy.jpg');
+      }
+    </style>
 <body>
-    <h2>Paslon BEM</h2>
-	<form method="post" action="<?= base_url() ?>pemilwa/add_vote_bpm">
-    <ul>
-    <?php foreach ($bpm as $b) { ?>
-        <li>
-            <p><img src="<?= base_url() ?>/assets/image/bpm/<?= $b->gambar ?>"></p>
-            <p><?= $b->nama_lengkap ?></p>        
-            <p><?= $prodi[$b->id_prodi - 1]->prodi ?></p>
-            <!-- kalo bisa dibuat drop down untuk visi misi, kalo nggak ya dihapus gakpapa atau gimana -->
-            <p><?= $b->visi ?></p>
-            <p><?= $b->misi ?></p>
-            <input type="radio" name="dipilih" value="<?= $b->no_urut ?>">Pilih
-        </li>
-    <?php } ?>
-		<input type="submit">
-        </ul>
-    </form>
-
-
+<div id="kontainer">
+        <div class="row">
+            <div class="col-sm-12 text-center" id="TempatTulisan">
+                <h1 id="Selamat"><img class="hidden-xs hidden-sm" src="assets/image/poros1.png">Selamat Datang di Website Pemilwa 2021<img class="hidden-xs hidden-sm" src="assets/image/ub1.png"></h1>
+                <h4 id="Tulisan">"Silahkan Tentukan Pilihan Anda!"</h4></div>
+        </div>
+<div class="container">
+<h3>Calon DPM </h3>
+<form method="post" action="<?= base_url() ?>pemilwa/add_vote_bpm">
+<div class="box" id="databem">
+      
+<div class="row">
+<?php foreach ($bpm as $b) { ?>
+              <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+              <img src="<?= base_url() ?>/assets/image/bpm/<?= $b->gambar ?>">
+              </div>
+              <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 column">
+                <h5 id="pasangan"> <?= $b->nama_lengkap ?></h5>
+              </div>
+              <div class="col-lg-10 col-lg-offset-1 col-md-6 col-md-offset-3 col-sm-7 col-sm-offset-3 column">
+                  <p id="prodi"><?= $prodi[$b->id_prodi - 1]->prodi ?></p>
+              </div>
+              <div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-7 col-sm-offset-3 column">
+              </div>
+          </div>		
+        <h5 id="pasangan">Visi</h5>
+        <p><?= $b->visi ?></p>
+        <h5 id="pasangan">Misi</h5>
+        <p><?= $b->misi ?></p>
+        <?php }?>
+              <div>
+                <input type="radio" name="dipilih" value="<?= $b->no_urut ?>" required>
+              
+              </div>
+</div>
+<div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-7 col-sm-offset-3 column">
+  <button class="btn btn-primary" type="submit" id="submit" name="Vote">Vote!</button> 
+</div>
+</form>
+</div>
 </body>
 
 </html>
