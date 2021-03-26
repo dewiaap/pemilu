117e38270eb330835e10e960349e88b914c34ccc
Bahrum <81005654+bahrum12@users.noreply.github.com>
2021-03-26 17:25:38 +0700

    Update vote_paslon.php

--- application/views/user/vote_paslon.php
+++ application/views/user/vote_paslon.php
@@ -48,8 +48,6 @@
               <div class="col-lg-11 col-lg-offset-1 col-md-6 col-md-offset-3 col-sm-7 col-sm-offset-3 column">
                   <p id="prodi"> <?= $prodi[$b->id_prodi_ketua - 1]->prodi ?> & <?= $prodi[$b->id_prodi_wakil - 1]->prodi ?></p>
               </div>
-              <div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-7 col-sm-offset-3 column">
-              </div>
           </div>		
         <h5 id="pasangan">Visi</h5>
         <p><?= $b->visi?></p>
